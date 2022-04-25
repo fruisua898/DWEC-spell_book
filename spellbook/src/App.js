6e34eb64f4230f32274395e62a8789685877c2e2
Fran <fruiz.alumno@iesrafaelalberti.es>
2022-04-25 18:26:18 +0200

    esqueleto

---
+++ spellbook/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+       
+        <p>
+          Bienvenido a SpellBook
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
